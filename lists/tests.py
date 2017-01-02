fcdea3f2b04b7c6ece947d9bf9734664b636f0e0
Pando <ndjman7@gmail.com>
2017-01-02 15:01:05 +0900

    템플릿에 POST요청으로 데이터 처리

--- lists/tests.py
+++ lists/tests.py
@@ -2,11 +2,15 @@ from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
-
+import re
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
+
+    def remove_csrf(self, origin):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', origin)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -15,5 +19,23 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = self.remove_csrf(render_to_string('home.html', request=request))
+        response_decode = self.remove_csrf(response.content.decode())
+        self.assertEqual(response_decode, expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = self.remove_csrf(render_to_string(
+            'home.html',
+            {'new_item_text': '신규 작업 아이템'},
+            request=request,
+        ))
+        response_decode = self.remove_csrf(response.content.decode())
+
+        self.assertEqual(response_decode, expected_html)
