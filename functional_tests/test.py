a6275752ec90da46d445b03b9ec776879688ac30
Pando <ra@Pandoui-MacBook-Pro.local>
2017-01-09 13:28:42 +0900

    두 개 폼을 위한 URL 적용, views와 tests에 있는 코드 이동, 신규 URL

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -17,7 +17,6 @@ class NewVisitorTest(LiveServerTestCase):
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        print([row.text for row in rows])
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
