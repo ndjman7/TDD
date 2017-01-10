4f334e3b746af9b9ec2b6e3d8c85f76edda10309
ndjman7 <ndjman7@gmail.com>
2017-01-10 21:14:08 +0900

    superlists urls.py에 include를 함.

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from lists import views
 
 
 urlpatterns = [
-    url(r'^', include('lists.urls')),
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/', include('lists.urls')),
     url(r'^admin/', admin.site.urls),
 ]
